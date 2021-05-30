dae5f076ecf759afa0969cede5ea1ce9efd67fc3
Helder Santos <heldersantos@bemol.onmicrosoft.com>
2021-05-30 15:53:58 -0400

    maximizando, minimizando e ouvindo alterações
    Signed-off-by: Helder Santos <heldersantos@bemol.onmicrosoft.com>

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const { app, BrowserWindow } = require("electron");
 const url = require("url");
 const path = require("path");
 
+if (process.env.NODE_ENV === "development") {
+  require("electron-reload")(__dirname); //hot reload
+}
+
 let mainWindow;
 
 function createWindows() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
 
   let file = url.format({
     pathname: path.join(__dirname, "index.html"),
@@ -14,6 +26,9 @@ function createWindows() {
   });
 
   mainWindow.loadURL(file);
+  mainWindow.openDevTools();
+
+  mainWindow.on("maximize", () => console.log("Maximizado"));
 }
 
 app.on("ready", createWindows);
---
+++ renderer.js
@@ -0,0 +1,28 @@
+const { remote } = require("electron");
+
+// requisita uma instancia da janela atual
+const mainWindow = remote.BrowserWindow.getFocusedWindow();
+
+let minimizar = document.getElementById("minimizar");
+minimizar.addEventListener("click", function (e) {
+  e.preventDefault();
+  mainWindow.minimize();
+});
+
+let maximizar = document.getElementById("maximizar");
+maximizar.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (mainWindow.isMaximized()) {
+    mainWindow.unmaximize();
+    maximizar.textContent = "maximizar";
+  } else {
+    mainWindow.maximize();
+    maximizar.textContent = "restaurar";
+  }
+});
+
+let fechar = document.getElementById("fechar");
+fechar.addEventListener("click", function (e) {
+  e.preventDefault();
+  mainWindow.close();
+});
